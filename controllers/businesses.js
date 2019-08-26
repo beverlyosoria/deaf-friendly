d4a00d165c2e4414d44ed5caa89f37cbef2eb8f5
beverlyosoria <bevosoria@gmail.com>
2019-08-26 00:41:54 -0500

    adds Oauth

--- controllers/businesses.js
+++ controllers/businesses.js
@@ -1,9 +1,15 @@
 var Business = require("../models/business");
+var User = require("../models/user");
 
 module.exports = {
   index
 };
 
 function index(req, res) {
-  res.render("businesses/index", { title: "Business" });
+  User.findById(req.params.id, function(err, user) {
+    res.render("businesses/index", {
+      user: req.user,
+      name: req.query.name
+    });
+  });
 }
